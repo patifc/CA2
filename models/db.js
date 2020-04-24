484c8bb953c359393532e332fd750865f40a83db
patifc <patifc@gmail.com>
2020-04-24 15:49:55 +0000

    My puppy's adoption

--- models/db.js
+++ models/db.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 //connect to mongodb
-mongoose.connect(process.env.MONGODB_URL , 
-    { useUnifiedTopology: true, useNewUrlParser: true }, (err) => {
+mongoose.connect(process.env.MONGODB_URL, { useUnifiedTopology: true, useNewUrlParser: true }, (err) => {
  if (!err) { console.log('MongoDB Connection Succeeded.') }
     else { console.log('Error in DB connection : ' + err) }
 });
