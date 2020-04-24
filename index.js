04413ab752be8b8603af6440daf12a579307a099
patifc <patifc@gmail.com>
2020-04-24 16:25:34 +0000

    deploying to heroku

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.set('views', path.join(__dirname, '/views/'));
 app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }));
 app.set('view engine', 'hbs');
 
-app.listen(4000, () => {
+app.listen(3000, () => {
     console.log('Express server started at port : 3000');
 });
 
--- models/db.js
+++ models/db.js
@@ -1,18 +1,11 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-/*/connect to mongodb
-mongoose.connect(process.env.MONGODB_URL, { useUnifiedTopology: true, useNewUrlParser: true }, (err) => {
+//connect to mongodb
+mongoose.connect(process.env.MONGODB_URL,
+ { useUnifiedTopology: true, useNewUrlParser: true }, (err) => {
  if (!err) { console.log('MongoDB Connection Succeeded.') }
     else { console.log('Error in DB connection : ' + err) }
-});*/
-mongoose.connect(process.env.MONGODB_URL);
-mongoose.connection.on('error', (err) => { 
-    console.log('Mongodb Error: ', err); 
-    process.exit();
-});
-mongoose.connection.on('connected', () => { 
-    console.log('MongoDB is successfully connected');
 });
 
 require('./dog.model');
