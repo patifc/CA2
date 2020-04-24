d28650c948e427226b2f3cb5319cb0961af3a8b3
patifc <patifc@gmail.com>
2020-04-24 15:54:04 +0000

    My puppy's adoption

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.set('views', path.join(__dirname, '/views/'));
 app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }));
 app.set('view engine', 'hbs');
 
-app.listen(3000, () => {
+app.listen(4000, () => {
     console.log('Express server started at port : 3000');
 });
 
